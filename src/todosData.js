12123427f599ff66da77305d79fcc48545e04f17
fangannie <39583104+fangannie@users.noreply.github.com>
2019-11-13 18:33:17 -0600

    Add files via upload

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Finish illustrating comic #5",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Read textbook pgs 156 - 180",
+        completed: false
+    },
+    {
+        id: 3,
+        text: "Drop off package at Kim's",
+        completed: false
+    },
+    {
+        id: 4,
+        text: "Finish physics problem set #8",
+        completed: true
+    },
+    {
+        id: 5,
+        text: "Catch up on How to Get Away with Murder",
+        completed: false
+    }
+]
+
+export default todosData
